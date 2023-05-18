bc41f901cdeae292bca10b049e88d8a58be3d725
Odame <kwesi.danquah@daaseng.com>
2023-05-18 01:30:25 +0100

    configure websockets and queues

--- docker-files/entrypoint.sh
+++ docker-files/entrypoint.sh
@@ -15,14 +15,26 @@ else
     echo "env file exists"
 fi
 
-php artisan migrate
-php artisan key:generate
-php artisan optimize:clear
-php artisan artisan db:seed
+role=${CONTAINER_ROLE:-app}
 
-php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+# for only the php app
+if [ "$role" = "app" ]; then
+    php artisan migrate
+    php artisan key:generate
+    php artisan optimize:clear
+    php artisan artisan db:seed
 
-# execute default docker php entry point
-exec docker-php-entrypoint "$@"
+    php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+
+    # execute default docker php entry point
+    exec docker-php-entrypoint "$@"
+elif [ "$role" = "queue" ]; then
+    echo "Running the queue ..."
+    php /var/www/artisan queue:work --verbose --tries=3 --timeout=180
+
+elif [ "$role" = "websocket" ]; then
+    echo "Running the websocket server ..."
+    php artisan websocket:serve
+fi
 
 
